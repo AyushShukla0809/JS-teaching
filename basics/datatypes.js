f33c70c7222e188ad6c4140c3f770fc41d560a97
Ayush Shukla <153834660+AyushShukla0809@users.noreply.github.com>
2024-05-19 16:25:34 +0000

    secondSave

---
+++ basics/datatypes.js
@@ -0,0 +1,10 @@
+console.log("data types");
+let age = 18
+let isValid= true
+console.log(typeof(age))
+//bigint is used to store very large number
+console.log(typeof(isValid))
+console.log(typeof(null))
+// null is a data type and it is said as standalone value
+console.log(typeof(undefined));
+// value is not assigned that will be undefined
---
+++ basics/variable.js
@@ -0,0 +1,10 @@
+console.log("Variable Concept")
+let nam= "Ayush"
+// Prefer not to use var
+// because of issue in block scope and functional scope
+ const age=24
+var height=6
+nam = "Shukla"
+height= 6.1
+console.log([nam,age,height])
+console.table([nam,age,height])
