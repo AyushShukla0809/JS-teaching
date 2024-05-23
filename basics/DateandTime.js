3ceb9934ef7920bf04aed9174fb1aaa5e5d33f0c
Ayush Shukla <153834660+AyushShukla0809@users.noreply.github.com>
2024-05-23 14:50:07 +0000

    Array is got commited

--- basics/DateandTime.js
+++ basics/DateandTime.js
@@ -29,3 +29,40 @@ console.log(Myarr.slice(2,4));// Slice will not include 4th amd also not affect 
 console.log(Myarr);
 console.log(Myarr.splice(2,4)); // Splice returns the array with including 4 values and affect the old array
 console.log(Myarr);
+let Marvels= ["Captain America","IronMan","Thor","Loki"]
+let Dcs= ["Super Man","WonderWomen","BatMan","Flash"];
+console.log(Marvels);
+console.log(Dcs);
+console.log("next command");
+console.log(Marvels.push(Dcs)); // It prints total number after pushing Dc as single element %
+console.log("next command");
+let all = Marvels.push(Dcs);
+console.log(all); // here it becomes 6
+console.log("next command");
+console.log(Marvels); // here it prints all the elements including Marvel and Dcs(as single element) 
+console.log("above marvels" );
+console.log(Marvels.concat(Dcs));
+let anew = Marvels.concat(Dcs); // it will prints Marvels element as updated and Dcs element in perfect in indivdual manner
+console.log(".....");
+console.log(anew);
+const allgiven = [...Marvels, ...Dcs];
+console.log(allgiven); // It works same as concatenation of arrays
+// Spread Out
+let dsa=anew.flat(Infinity);// It makes all the element like s=array within array as distinct elements
+console.log(dsa);
+console.log("same");
+// console.log(allgiven.flat(Infinity));// same as below
+// let finar= allgiven.flat(Infinity); // it made it all indivdual elements
+// console.log(finar);
+ // ARRAY IS
+console.log(Array.isArray("Loki"));// false
+console.log(Array.isArray(anew)); // True
+console.log(Array.isArray("anew")); // false( treat it as string)
+// ARRAY FROM
+console.log(Array.from("SuuperMann"));
+let absd= Array.from("fjwddfofaoo");
+console.log(absd);
+//ARRAY OF
+let sc1=100;
+let sc=6;
+console.log(Array.of(sc1,sc));
