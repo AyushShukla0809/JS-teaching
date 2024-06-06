27302cd82fdd430fb83e525472a83a3abe997b46
Ayush Shukla <153834660+AyushShukla0809@users.noreply.github.com>
2024-06-06 14:21:35 +0000

    till19

--- basics/Function.js
+++ basics/Function.js
@@ -12,7 +12,28 @@ console.log("H");
  function AdditionTwoNumbers(num1,num2){
     console.log(num1 + num2);
  }
- AdditionTwoNumbers(4,6);
- const res= AdditionTwoNumbers(5,6);
-console.log('Result of the function is ${res}');
 
+
+ function AddTwo(n1,n2){
+   console.log("Sum before return will print ");
+   // let result = n1+n2;
+   // return result; // once funnction returnsomething it will not execute further statements
+   return n1 + n2;
+   // console.log("Sum of numbers after after return will not printed") // It will not get printed after return.
+ }
+
+
+ const Ans =  AddTwo(5,2);
+ console.log("Return value Output is like " , Ans);
+ AdditionTwoNumbers(4,6)
+ const res= AdditionTwoNumbers(5,6);
+ console.log("first");
+ console.log(res); // res do not store any returned value so it is UNDEFINED here
+ console.log("second");
+//  console.log("2",res);
+// console.log('Result of the function is ${res}');
+
+function LoginMessage(UserName){
+return '${UserName} is recently logged in'
+}
+console.log(LoginMessage("Hitesh"))
